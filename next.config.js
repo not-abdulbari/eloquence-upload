1b0b1d990e69348aa74b11b0b4a8795e0f3c102f
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-10-18 16:20:04 +0530

    switch to standalone SSR

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
