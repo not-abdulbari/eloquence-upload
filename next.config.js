d544f1831bde4642d4f9895a2c83a2b67e2859e0
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-10-18 19:34:02 +0530

    config mjs file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Add this line
+  output: 'export', // Add this line
   eslint: {
     ignoreDuringBuilds: true,
   },
